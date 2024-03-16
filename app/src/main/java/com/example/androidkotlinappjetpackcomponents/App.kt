a066d3aeeea13a9050f3dacedd94d0bc1d919a29
Yogesh Aswar <yogeshaswar46@gmail.com>
2024-03-16 21:19:47 +0530

    added MainActivity code

---
+++ app/src/main/java/com/example/androidkotlinappjetpackcomponents/App.kt
@@ -0,0 +1,15 @@
+package com.example.androidkotlinappjetpackcomponents
+
+import android.app.Application
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initiate()
+    }
+
+    private fun initiate() {
+
+    }
+
+}
--- app/src/main/java/com/example/androidkotlinappjetpackcomponents/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinappjetpackcomponents/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.androidkotlinappjetpackcomponents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.androidkotlinappjetpackcomponents.api.ApiService
+import com.example.androidkotlinappjetpackcomponents.api.RetrofitHelper
+import com.example.androidkotlinappjetpackcomponents.models.Posts
+import com.example.androidkotlinappjetpackcomponents.repository.PostRepository
+import com.example.androidkotlinappjetpackcomponents.viewmodels.PostViewModel
+import com.example.androidkotlinappjetpackcomponents.viewmodels.PostViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var postViewModel: PostViewModel
+    lateinit var repository: PostRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apiService = RetrofitHelper.getRetrofitInstance().create(ApiService::class.java)
+        repository = PostRepository(apiService)
+        postViewModel = ViewModelProvider(this, PostViewModelFactory(repository)).get(PostViewModel::class.java)
+
+        postViewModel.posts.observe(this, Observer { posts: Posts ->
+            Log.d("TAG", "ApiResponse: ${posts.get(0)}")
+        })
+
     }
 }
